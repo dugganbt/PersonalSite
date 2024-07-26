82fb75c770c345fe0f790b3185ede07af232d14c
dugganbt <99730314+dugganbt@users.noreply.github.com>
2024-07-26 13:08:41 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+# initialize flask application
+app = Flask(__name__)
+
+
+# home screen route
+@app.route("/")
+def run_website():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
